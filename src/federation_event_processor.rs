f42f7df9f8038e85cd65eca1aca5f09ce0bd9ea9
Justin <jumoell@protonmail.com>
2025-03-25 16:58:55 -0500

    refactor: add federation event processor

---
+++ src/federation_event_processor.rs
@@ -0,0 +1,271 @@
+use std::fmt;
+
+use fedimint_core::{anyhow, config::FederationId};
+use fedimint_eventlog::{EventKind, EventLogId};
+use ln_gateway::rpc::{rpc_client::GatewayRpcClient, FederationInfo, PaymentLogPayload};
+use serde_json::Value;
+use tokio_postgres::Client;
+use tracing::warn;
+
+use crate::{
+    parse_log_id, CompleteLightningPaymentSucceeded, DbConnection, LNv1IncomingPaymentFailed,
+    LNv1IncomingPaymentStarted, LNv1IncomingPaymentSucceeded, LNv1OutgoingPaymentFailed,
+    LNv1OutgoingPaymentStarted, LNv1OutgoingPaymentSucceeded,
+};
+
+pub(crate) struct FederationEventProcessor {
+    federation_id: FederationId,
+    federation_name: String,
+    max_log_id: i64,
+    pg_client: Client,
+    gw_client: GatewayRpcClient,
+    outgoing_payment_started_count: u64,
+    outgoing_payment_succeeded_count: u64,
+    outgoing_payment_failed_count: u64,
+    incoming_payment_started_count: u64,
+    incoming_payment_succeeded_count: u64,
+    incoming_payment_failed_count: u64,
+    complete_lightning_payment_succeeded_count: u64,
+}
+
+impl fmt::Display for FederationEventProcessor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Federation: {}\nMax Log ID: {}\n\
+            Outgoing Payments - Started: {}, Succeeded: {}, Failed: {}\n\
+            Incoming Payments - Started: {}, Succeeded: {}, Failed: {}\n\
+            Complete Lightning Payments Succeeded: {}",
+            self.federation_name,
+            self.max_log_id,
+            self.outgoing_payment_started_count,
+            self.outgoing_payment_succeeded_count,
+            self.outgoing_payment_failed_count,
+            self.incoming_payment_started_count,
+            self.incoming_payment_succeeded_count,
+            self.incoming_payment_failed_count,
+            self.complete_lightning_payment_succeeded_count
+        )
+    }
+}
+
+impl FederationEventProcessor {
+    pub async fn new(
+        fed_info: FederationInfo,
+        db_conn: DbConnection,
+        gw_client: GatewayRpcClient,
+    ) -> anyhow::Result<FederationEventProcessor> {
+        let pg_client = db_conn.connect().await?;
+        let max_log_id = Self::get_max_log_id(&pg_client, fed_info.federation_id).await?;
+        Ok(Self {
+            federation_id: fed_info.federation_id,
+            federation_name: fed_info
+                .federation_name
+                .expect("No federation name provided"),
+            max_log_id,
+            pg_client,
+            gw_client,
+            outgoing_payment_started_count: 0,
+            outgoing_payment_succeeded_count: 0,
+            outgoing_payment_failed_count: 0,
+            incoming_payment_started_count: 0,
+            incoming_payment_succeeded_count: 0,
+            incoming_payment_failed_count: 0,
+            complete_lightning_payment_succeeded_count: 0,
+        })
+    }
+
+    async fn get_max_log_id(
+        pg_client: &Client,
+        federation_id: FederationId,
+    ) -> anyhow::Result<i64> {
+        let query = "
+            SELECT MAX(log_id)
+            FROM (
+                SELECT log_id FROM lnv1_outgoing_payment_started WHERE federation_id = $1
+                UNION ALL
+                SELECT log_id FROM lnv1_outgoing_payment_succeeded WHERE federation_id = $1
+                UNION ALL
+                SELECT log_id FROM lnv1_outgoing_payment_failed WHERE federation_id = $1
+                UNION ALL
+                SELECT log_id FROM lnv1_incoming_payment_started WHERE federation_id = $1
+                UNION ALL
+                SELECT log_id FROM lnv1_incoming_payment_succeeded WHERE federation_id = $1
+                UNION ALL
+                SELECT log_id FROM lnv1_incoming_payment_failed WHERE federation_id = $1
+                UNION ALL
+                SELECT log_id FROM lnv1_complete_lightning_payment_succeeded WHERE federation_id = $1
+            ) AS combined_log_ids
+        ";
+
+        let rows = pg_client
+            .query(query, &[&federation_id.to_string()])
+            .await?;
+        if let Some(row) = rows.get(0) {
+            let max_log_id: Option<i64> = row.get(0);
+            if let Some(max_log_id) = max_log_id {
+                return Ok(max_log_id);
+            }
+        }
+
+        Ok(0)
+    }
+
+    pub async fn process_events(&mut self) -> anyhow::Result<()> {
+        let payment_log = self
+            .gw_client
+            .payment_log(PaymentLogPayload {
+                end_position: None,
+                pagination_size: usize::MAX,
+                federation_id: self.federation_id,
+                event_kinds: vec![],
+            })
+            .await?;
+        for (log_id, kind, module, timestamp, value) in payment_log.0 {
+            if parse_log_id(&log_id) <= self.max_log_id {
+                break;
+            }
+
+            match module {
+                Some((module, _)) if module.as_str() == "ln" => {
+                    self.handle_lnv1(log_id, kind, timestamp, value).await?;
+                }
+                Some((module, _)) => {
+                    warn!(module = %module, "Unsupported module");
+                }
+                None => {
+                    warn!("No module provided");
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn handle_lnv1(
+        &mut self,
+        log_id: EventLogId,
+        kind: EventKind,
+        timestamp: u64,
+        value: Value,
+    ) -> anyhow::Result<()> {
+        let kind = Self::parse_event_kind(format!("{kind:?}"));
+        match kind.as_str() {
+            "outgoing-payment-started" => {
+                let outgoing_payment_started_event: LNv1OutgoingPaymentStarted =
+                    serde_json::from_value(value).expect("Could not parse event");
+                outgoing_payment_started_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.outgoing_payment_started_count += 1;
+            }
+            "outgoing-payment-succeeded" => {
+                let outgoing_payment_succeeded_event: LNv1OutgoingPaymentSucceeded =
+                    serde_json::from_value(value).expect("Could not parse event");
+                outgoing_payment_succeeded_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.outgoing_payment_succeeded_count += 1;
+            }
+            "outgoing-payment-failed" => {
+                let outgoing_payment_failed_event: LNv1OutgoingPaymentFailed =
+                    serde_json::from_value(value).expect("Could not parse event");
+                outgoing_payment_failed_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.outgoing_payment_failed_count += 1;
+            }
+            "incoming-payment-started" => {
+                let incoming_payment_started_event: LNv1IncomingPaymentStarted =
+                    serde_json::from_value(value).expect("Could not parse event");
+                incoming_payment_started_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.incoming_payment_started_count += 1;
+            }
+            "incoming-payment-succeeded" => {
+                let incoming_payment_succeeded_event: LNv1IncomingPaymentSucceeded =
+                    serde_json::from_value(value).expect("Could not parse event");
+                incoming_payment_succeeded_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.incoming_payment_succeeded_count += 1;
+            }
+            "incoming-payment-failed" => {
+                let incoming_payment_failed_event: LNv1IncomingPaymentFailed =
+                    serde_json::from_value(value).expect("Could not parse event");
+                incoming_payment_failed_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.incoming_payment_failed_count += 1;
+            }
+            "complete-lightning-payment-succeeded" => {
+                let complete_lightning_payment_succeeded_event: CompleteLightningPaymentSucceeded =
+                    serde_json::from_value(value).expect("Could not parse event");
+                complete_lightning_payment_succeeded_event
+                    .insert(
+                        &self.pg_client,
+                        &log_id,
+                        timestamp,
+                        &self.federation_id,
+                        self.federation_name.clone(),
+                    )
+                    .await?;
+                self.complete_lightning_payment_succeeded_count += 1;
+            }
+            event => {
+                warn!(?event, "Unrecognized event");
+            }
+        }
+
+        Ok(())
+    }
+
+    // TODO: Remove this once EventKind can be parsed correctly
+    fn parse_event_kind(input: String) -> String {
+        if let Some(start) = input.find('(') {
+            if let Some(end) = input.rfind(')') {
+                let extracted = &input[start + 2..end - 1]; // Skip `("` and `")`
+                return extracted.to_string();
+            }
+        }
+
+        panic!("Malformatted String");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use federation_event_processor::FederationEventProcessor;
 use fedimint_core::{anyhow, util::SafeUrl};
 use fedimint_eventlog::EventLogId;
 use fedimint_logging::TracingSetup;
@@ -6,13 +7,14 @@ use incoming::{
     CompleteLightningPaymentSucceeded, LNv1IncomingPaymentFailed, LNv1IncomingPaymentStarted,
     LNv1IncomingPaymentSucceeded,
 };
-use ln_gateway::rpc::{rpc_client::GatewayRpcClient, PaymentLogPayload};
+use ln_gateway::rpc::rpc_client::GatewayRpcClient;
 use outgoing::{
     LNv1OutgoingPaymentFailed, LNv1OutgoingPaymentStarted, LNv1OutgoingPaymentSucceeded,
 };
 use tokio_postgres::{Client, NoTls};
-use tracing::{error, info, warn};
+use tracing::{error, info};
 
+mod federation_event_processor;
 mod incoming;
 mod outgoing;
 
@@ -51,225 +53,59 @@ struct GatewayETLOpts {
 async fn main() -> anyhow::Result<()> {
     TracingSetup::default().init()?;
     let opts = GatewayETLOpts::parse();
-    let client = GatewayRpcClient::new(opts.gateway_addr, Some(opts.password));
+    let conn = DbConnection::from_opts(&opts);
 
-    let pg_client = connect_pg(opts.db_host, opts.db_user, opts.db_password, opts.db_name).await?;
-
-    // This needs to be per federation
-    let max_log_id = get_max_log_id(&pg_client).await?;
-
-    info!(?max_log_id, "Getting gateway info...");
+    let client = GatewayRpcClient::new(opts.gateway_addr.clone(), Some(opts.password.clone()));
     let info = client.get_info().await?;
     for fed_info in info.federations {
-        {
-            let federation_id = fed_info.federation_id;
-            let federation_name = fed_info.federation_name.clone().expect("should be present");
+        let client = GatewayRpcClient::new(opts.gateway_addr.clone(), Some(opts.password.clone()));
+        let mut processor = FederationEventProcessor::new(fed_info, conn.clone(), client).await?;
+        processor.process_events().await?;
 
-            let payment_log = client
-                .payment_log(PaymentLogPayload {
-                    end_position: None,
-                    pagination_size: usize::MAX,
-                    federation_id,
-                    event_kinds: vec![],
-                })
-                .await?;
-
-            let mut outgoing_payment_started = 0;
-            let mut outgoing_payment_succeeded = 0;
-            let mut outgoing_payment_failed = 0;
-            let mut incoming_payment_started = 0;
-            let mut incoming_payment_succeeded = 0;
-            let mut incoming_payment_failed = 0;
-            let mut complete_lightning_payment_succeeded = 0;
-            for (log_id, kind, module, timestamp, value) in payment_log.0 {
-                if parse_log_id(&log_id) <= max_log_id {
-                    break;
-                }
-
-                let kind = parse_event_kind(format!("{kind:?}"));
-                let module = module.expect("Should be present").0.as_str().to_string();
-                match (kind.as_str(), module.as_str()) {
-                    ("outgoing-payment-started", "ln") => {
-                        let outgoing_payment_started_event: LNv1OutgoingPaymentStarted =
-                            serde_json::from_value(value).expect("Could not parse event");
-                        outgoing_payment_started_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        outgoing_payment_started += 1;
-                    }
-                    ("outgoing-payment-succeeded", "ln") => {
-                        let outgoing_payment_succeeded_event: LNv1OutgoingPaymentSucceeded =
-                            serde_json::from_value(value).expect("Could not parse event");
-                        outgoing_payment_succeeded_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        outgoing_payment_succeeded += 1;
-                    }
-                    ("outgoing-payment-failed", "ln") => {
-                        let outgoing_payment_failed_event: LNv1OutgoingPaymentFailed =
-                            serde_json::from_value(value).expect("Could not parse event");
-                        outgoing_payment_failed_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        outgoing_payment_failed += 1;
-                    }
-                    ("incoming-payment-started", "ln") => {
-                        let incoming_payment_started_event: LNv1IncomingPaymentStarted =
-                            serde_json::from_value(value).expect("Could not parse event");
-                        incoming_payment_started_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        incoming_payment_started += 1;
-                    }
-                    ("incoming-payment-succeeded", "ln") => {
-                        let incoming_payment_succeeded_event: LNv1IncomingPaymentSucceeded =
-                            serde_json::from_value(value).expect("Could not parse event");
-                        incoming_payment_succeeded_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        incoming_payment_succeeded += 1;
-                    }
-                    ("incoming-payment-failed", "ln") => {
-                        let incoming_payment_failed_event: LNv1IncomingPaymentFailed =
-                            serde_json::from_value(value).expect("Could not parse event");
-                        incoming_payment_failed_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        incoming_payment_failed += 1;
-                    }
-                    ("complete-lightning-payment-succeeded", "ln") => {
-                        let complete_lightning_payment_succeeded_event: CompleteLightningPaymentSucceeded = serde_json::from_value(value).expect("Could not parse event");
-                        complete_lightning_payment_succeeded_event
-                            .insert(
-                                &pg_client,
-                                &log_id,
-                                timestamp,
-                                &federation_id,
-                                federation_name.clone(),
-                            )
-                            .await?;
-                        complete_lightning_payment_succeeded += 1;
-                    }
-                    (kind, module) => {
-                        warn!(?kind, ?module, "Unknown event kind or module");
-                    }
-                }
-            }
-
-            let total_events = outgoing_payment_started
-                + outgoing_payment_succeeded
-                + outgoing_payment_failed
-                + incoming_payment_started
-                + incoming_payment_succeeded
-                + incoming_payment_failed
-                + complete_lightning_payment_succeeded;
-            info!(?total_events, ?outgoing_payment_started, ?outgoing_payment_succeeded, ?outgoing_payment_failed, ?incoming_payment_started, ?incoming_payment_succeeded, ?incoming_payment_failed, ?complete_lightning_payment_succeeded, federation_name = %fed_info.federation_name.clone().expect("Name should be present"), "Total Events");
-        }
+        info!("{processor}");
     }
 
     Ok(())
 }
 
-// TODO: add federation ID to where clause
-async fn get_max_log_id(pg_client: &Client) -> anyhow::Result<i64> {
-    let query = "
-        SELECT MAX(log_id)
-        FROM (
-            SELECT log_id FROM lnv1_outgoing_payment_started 
-            UNION ALL
-            SELECT log_id FROM lnv1_outgoing_payment_succeeded
-            UNION ALL
-            SELECT log_id FROM lnv1_outgoing_payment_failed
-            UNION ALL
-            SELECT log_id FROM lnv1_incoming_payment_started
-            UNION ALL
-            SELECT log_id FROM lnv1_incoming_payment_succeeded
-            UNION ALL
-            SELECT log_id FROM lnv1_incoming_payment_failed
-            UNION ALL
-            SELECT log_id FROM lnv1_complete_lightning_payment_succeeded
-        ) AS combined_log_ids
-    ";
-
-    let rows = pg_client.query(query, &[]).await?;
-    if let Some(row) = rows.get(0) {
-        let max_log_id: Option<i64> = row.get(0);
-        if let Some(max_log_id) = max_log_id {
-            return Ok(max_log_id);
-        }
-    }
-
-    Ok(0)
-}
-
-async fn connect_pg(
+#[derive(Debug, Clone)]
+struct DbConnection {
     db_host: String,
     db_user: String,
     db_password: String,
     db_name: String,
-) -> anyhow::Result<Client> {
-    info!("Connecting to database...");
-    let (pg_client, pg_connection) = tokio_postgres::connect(
-        format!("host={db_host} user={db_user} password={db_password} dbname={db_name}").as_str(),
-        NoTls,
-    )
-    .await?;
-
-    tokio::spawn(async move {
-        if let Err(err) = pg_connection.await {
-            error!(?err, "Postgres connection error");
-        }
-    });
-
-    Ok(pg_client)
 }
 
-// TODO: Remove this once EventKind can be parsed correctly
-fn parse_event_kind(input: String) -> String {
-    if let Some(start) = input.find('(') {
-        if let Some(end) = input.rfind(')') {
-            let extracted = &input[start + 2..end - 1]; // Skip `("` and `")`
-            return extracted.to_string();
+impl DbConnection {
+    fn from_opts(opts: &GatewayETLOpts) -> DbConnection {
+        DbConnection {
+            db_host: opts.db_host.clone(),
+            db_user: opts.db_user.clone(),
+            db_password: opts.db_password.clone(),
+            db_name: opts.db_name.clone(),
         }
     }
 
-    panic!("Malformatted String");
+    async fn connect(&self) -> anyhow::Result<Client> {
+        info!("Connecting to database...");
+        let (pg_client, pg_connection) = tokio_postgres::connect(
+            format!(
+                "host={} user={} password={} dbname={}",
+                self.db_host, self.db_user, self.db_password, self.db_name
+            )
+            .as_str(),
+            NoTls,
+        )
+        .await?;
+
+        tokio::spawn(async move {
+            if let Err(err) = pg_connection.await {
+                error!(?err, "Postgres connection error");
+            }
+        });
+
+        Ok(pg_client)
+    }
 }
 
 // TODO: Remove this once LogId can be used as a u64
